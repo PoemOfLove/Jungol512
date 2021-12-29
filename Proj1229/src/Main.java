97f24f1540c2ff62b202f804858d2200bb6c760f
Si7A-00 <Si7A-00@Si7C-02>
2021-12-29 10:46:13 +0900

    1st commit

---
+++ Proj1229/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		int weight = 49;
+		double gravity = 0.2683;
+		
+//		System.out.print(num1 + " * " + num2 + " = " + num1 * num2);
+
+		
+		System.out.printf("%d * %f = %f\n", weight, gravity, weight * gravity);
+		
+	}
+
+}
+
+// print / println / printf
